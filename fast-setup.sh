34ed2e442e624c5e36c7734aa023b887a11e16e4
Kevin Chen <kevin.zchn@gmail.com>
2021-12-16 18:18:52 +0800

    Trying to fix the tests.ke problem by cleaning up the dts file. Not yet tested.

--- fast-setup.sh
+++ fast-setup.sh
@@ -70,6 +70,7 @@ else
     mkdir -p renode-1.12.0
     tar xf  renode-1.12.0.linux-portable.tar.gz -C renode-1.12.0 --strip-components=1
     export PATH=$(pwd)/renode-1.12.0:$PATH
+    apt install python3-pip
     pip3 install -r ./renode-1.12.0/tests/requirements.txt
 fi
 
